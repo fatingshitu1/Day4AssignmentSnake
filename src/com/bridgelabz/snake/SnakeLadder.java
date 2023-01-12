be6296560584fd803a1a86619dff4ccbd196684f
shitalfating <shitalfating84@gmail.com>
2023-01-12 11:44:42 +0530

    welcome to uc2_roll_die

--- src/com/bridgelabz/snake/SnakeLadder.java
+++ src/com/bridgelabz/snake/SnakeLadder.java
@@ -2,8 +2,13 @@ package com.bridgelabz.snake;
 
 public class SnakeLadder {
     static int player1Position = 0;
-    
+    static int rollDie(){
+        int dice = ((int) (Math.random() * 10) % 6) +1;
+        return dice;
+    }
     public static void main(String[] args) {
+        int dice = rollDie();
+        System.out.println(dice);
 
     }
 }
